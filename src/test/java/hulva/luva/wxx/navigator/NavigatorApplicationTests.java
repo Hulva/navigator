d58e7e7c4a229e147d03bd5a9e380b2bb6739a37
Hulva Luva.H <lvsweet217@163.com>
2020-08-17 10:35:27 +0800

    init

---
+++ src/test/java/hulva/luva/wxx/navigator/NavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hulva.luva.wxx.navigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
